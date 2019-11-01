f69fa844d9e5ef2ef6f37eaf79cbd0bfff0671ab
Benjamin Maynard <36383062+benjamin-maynard@users.noreply.github.com>
2019-11-01 15:07:37 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g" | sed -e "s/\//-/g")
 REPO=$(echo ${GITHUB_REPOSITORY} | awk '{print tolower($0)}')
+REPOSHORTNAME=$(echo ${GITHUB_REPOSITORY} | awk -F'/' '{print $2}')
 
 echo ::set-output name=repo::$REPO
 echo ::set-output name=branch::$BRANCH
+echo ::set-output name=reposhortname::$REPOSHORTNAME
+
+
